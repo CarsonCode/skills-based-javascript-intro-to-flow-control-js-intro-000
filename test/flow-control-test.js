ee2ea265a9bf6d61acbcea6fe5ee8a8f1bc67940
Carson Garcia <carsongarcia037@gmail.com>
2021-02-20 22:13:39 +0000

    Automatically backed up by Learn

--- test/flow-control-test.js
+++ test/flow-control-test.js
@@ -24,9 +24,7 @@ describe('flow-control', () => {
   describe('basicTeenager', () => {
     it('should return "You are a teenager!" if the age is between 13-19', () => {
       expect(basicTeenager(13)).toEqual("You are a teenager!");
-      if(basicTeenager(13)).toEqual(13){
-        null
-      }
+      return "You are a teenager!"
     })
 
     it('should return undefined if the age is not between 13-19', () => {
